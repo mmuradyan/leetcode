f6bda1fbac9fdf4e2af7f72cc3eab4cb98569b93
Mikayel Muradyan <mmuradyan@gmail.com>
2020-09-14 02:25:37 -0700

    N/A

---
+++ solutions/1. Two Sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let pairs = {};
+    let arr_new = []
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] <= target){ //arr_new.push(nums[i]);
+            for(let j=0; j<nums.length; j++){
+                
+            }
+        }
+    }
+};
